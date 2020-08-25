339445efd0493abf8c9aea86f2b15246687555bd
Tyler Menezes <github@xanadu.ws>
2020-08-24 21:45:15 -0400

    Reduce slowdown from ChakraUI Image component

--- src/Atom/Image/index.js
+++ src/Atom/Image/index.js
@@ -1,1 +1,8 @@
-export { default } from '@chakra-ui/core/dist/Image';
+import React from 'react';
+import Box from 'topo/Atom/Box';
+
+export default function Image({ src, alt, ...props }) {
+  return (
+    <Box as="img" src={src} alt={alt || ''} {...props} />
+  );
+}
