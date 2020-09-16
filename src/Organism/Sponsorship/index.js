81711f64df8c541f69e90e6633a472a9f79e71f0
Bobcat <bobcat4848@gmail.com>
2020-09-16 12:36:58 -0500

    Data retrieval works

--- src/Organism/Sponsorship/index.js
+++ src/Organism/Sponsorship/index.js
@@ -8,38 +8,18 @@ import Content from 'topo/Molecule/Content';
 import { childrenOfType, makePureBox } from 'topo/_utils';
 import { useString, apiFetch } from 'topo/utils';
 
-export const CustomLinks = makePureBox('Custom Links');
-export const CustomText = makePureBox('CustomText');
-
-/*
-const query = `{
-  {
-    cms {
-      programs {
-        items {
-          sponsorPerks
-        }
-      }
-    }
-  }
-}`;
-*/
 
 const query = `{
   cms {
-    sites(where: { type: "Public", display_contains_all: "Footer" }) {
+    programs {
       items {
-        sys {
-          id
-        }
-        title
-        link
+        sponsorPerks
       }
     }
   }
 }`;
 
-const StandardLinks = () => {
+const SponsorTable = () => {
   const { data, error } = useSwr(
     query,
     apiFetch,
@@ -48,55 +28,35 @@ const StandardLinks = () => {
       revalidateOnReconnect: false,
     }
   );
-  const links = data?.cms?.sites?.items;
+  const sponsorData = data?.cms?.programs?.items;
+
+  console.log(sponsorData);
 
   return (
+    <Text>Sponsorship table</Text>
+    /*
     <List>
-      {!links ? (
+      {!sponsorData ? (
         <>
           <Item><Skelly /></Item>
           <Item><Skelly /></Item>
           <Item><Skelly /></Item>
         </>
-      ) : links.map(({ title, link, sys }) => (
-        <Item key={sys.id}><Link href={link} target="_blank" rel="noopener" key={link}>{title}</Link></Item>
+      ) : sponsorData.map(({ sponsorPerks }) => (
+        <Item><Link target="_blank" rel="noopener">{sponsorPerks}</Link></Item>
       ))}
     </List>
+    */
   );
+
 };
 
 const Sponsorship = forwardRef(({ children }, ref) => {
-  const cookiesLink = useString('legal.cookies', <Skelly />);
-  const ccpaLink = useString('legal.ccpa', <Skelly />);
-
-  const customLinks = childrenOfType(children, CustomLinks);
-  const customText = childrenOfType(children, CustomText);
 
   return (
     <Content ref={ref} role="contentinfo">
       <Grid templateColumns={{ base: '1fr', md: '6fr 3fr 3fr' }} color="current.textLight">
-        <Box gridRow={{ base: 3, md: 1 }} marginTop={{ base: 6, md: 0 }}>
-          <Box>
-            { customText.length > 0 ? customText : (
-              <Text>
-                Copyright &copy; 2009 &ndash; {(new Date()).getFullYear()} CodeDay.<br />
-                A 501(c)(3) nonprofit. <CopyText label="EIN: ">26-4742589</CopyText>.<br />
-                <Link href="tel:18886077763">(888) 607-7763</Link>
-              </Text>
-            )}
-          </Box>
-          <Box marginTop={4}>
-            <Link href="https://www.codeday.org/privacy" rel="noopener">{cookiesLink}</Link><br />
-            <Link href="https://www.codeday.org/privacy/controls" rel="noopener" target="_blank">{ccpaLink}</Link>
-          </Box>
-        </Box>
-        <Box gridRow={{ base: 2, md: 1 }} marginTop={{ base: customLinks.length > 0 && 6, md: 0 }}>
-          {customLinks.length > 0 && <Heading as="h2" fontSize="xl">More</Heading>}{customLinks}
-        </Box>
-        <Box>
-          <Heading as="h2" fontSize="xl">Resources</Heading>
-          <StandardLinks />
-        </Box>
+        <SponsorTable />
       </Grid>
     </Content>
   );
