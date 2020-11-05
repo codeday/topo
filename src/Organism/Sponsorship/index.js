bb591d63d4ed912b94da0c90a246e7c9beb930dc
Bobcat <bobcat4848@gmail.com>
2020-11-04 19:41:55 -0600

    GraphQL Working & Top Layout of Table Done

--- src/Organism/Sponsorship/index.js
+++ src/Organism/Sponsorship/index.js
@@ -2,13 +2,15 @@ import React, { forwardRef } from 'react';
 import useSwr from 'swr';
 import List, { Item } from 'topo/Atom/List';
 import Box, { Grid } from 'topo/Atom/Box';
+import Divider from 'topo/Atom/Divider';
+import { Flex } from "@chakra-ui/core"
 import Skelly from 'topo/Atom/Skelly';
 import Text, { Heading, Link, CopyText } from 'topo/Atom/Text';
 import Content from 'topo/Molecule/Content';
 import { childrenOfType, makePureBox } from 'topo/_utils';
 import { useString, apiFetch } from 'topo/utils';
 
-
+// GraphQL Query, pulls data from the sponsorship programs area
 const query = `{
   cms {
     programs {
@@ -19,7 +21,11 @@ const query = `{
   }
 }`;
 
+
+
 const SponsorTable = () => {
+
+  // Pull data from GraphQL Query from above
   const { data, error } = useSwr(
     query,
     apiFetch,
@@ -28,39 +34,68 @@ const SponsorTable = () => {
       revalidateOnReconnect: false,
     }
   );
-  const sponsorData = data?.cms?.programs?.items;
 
-  console.log(sponsorData);
+  // Let data be set equal to the variables levels and perkGroups respectively
+  const { levels, perkGroups } = data?.cms?.programs?.items[1]?.sponsorPerks || {};
+  const count = 0;
+  /*
+  var LevelsList;
+  for (var l in levels) {
+    var currentLevel = {};
 
+    LevelsList.push({name: l.name});
+  }*/
+
+  if (levels) {
+    Object.keys(levels).map(function(key, index) {
+      console.log(levels[key].name);
+    });
+  }
+
+
+  
+  // DEBUG: Print straight to console to ensure data is being queried correctly
+  console.log(levels, perkGroups);
+
+  // Return HTML
   return (
-    <Text>
+    <Box>
       <Heading as="h1">Virtual CodeDay Sponsorships</Heading>
       <Heading as="h4">{(new Date()).getFullYear()}-{(new Date()).getFullYear()+1} School Year</Heading>
-    </Text>
 
+      <List>
+        <Flex>
+        {!(levels && perkGroups) ? (
+          <>
+            <Item><Skelly /></Item>
+            <Item><Skelly /></Item>
+            <Item><Skelly /></Item>
+          </>
+        ) : Object.keys(levels).map((key, index) =>
+        <Item key={levels[key].name}>
+          <Box borderWidth="2px" rounded="lg" m={2} h="200px">
+            <Box borderWidth="2px" p={3} w="100%" roundedTopLeft="lg" roundedTopRight="lg" backgroundColor="#F0E5E6">
+              {levels[key].name}
+            </Box>
+            <Box p={3} w="100%">
+              <strong>${levels[key].amount}/{levels[key].amountInterval}</strong><br></br>
+              {levels[key].description}
+            </Box>
+          </Box>
 
+          <strong>Promotion</strong>
+          <Divider m="auto" w="90%"></Divider>
+        </Item>
 
-
-
-    /*
-    <List>
-      {!sponsorData ? (
-        <>
-          <Item><Skelly /></Item>
-          <Item><Skelly /></Item>
-          <Item><Skelly /></Item>
-        </>
-      ) : sponsorData.map(({ sponsorPerks }) => (
-        <Item><Link target="_blank" rel="noopener">{sponsorPerks}</Link></Item>
-      ))}
-    </List>
-    */
+        )}
+        </Flex>
+      </List>
+    </Box>
   );
-
 };
 
+// Actual Sponsorship Object that will be used in external pages
 const Sponsorship = forwardRef(({ children }, ref) => {
-
   return (
     <Content ref={ref} role="contentinfo">
       <SponsorTable />
@@ -68,6 +103,4 @@ const Sponsorship = forwardRef(({ children }, ref) => {
   );
 });
 
-Sponsorship.propTypes = Box.propTypes;
-Sponsorship.defaultProps = Box.defaultProps;
 export default Sponsorship;
