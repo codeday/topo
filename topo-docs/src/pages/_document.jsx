c180f7db61690d2f5beec360565859e4e137745c
Nexite <nikhil@gargmail.com>
2022-01-07 12:59:09 -0800

    add favicons

--- topo-docs/src/pages/_document.jsx
+++ topo-docs/src/pages/_document.jsx
@@ -10,6 +10,14 @@ class MyDocument extends Document {
             rel="stylesheet"
             key="google-font-Fira"
           />
+          <link rel="shortcut icon" href="/favicon.ico" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ff686b" />
+          <meta name="msapplication-TileColor" content="#ff686b" />
+          <meta name="theme-color" content="#ffffff" />
         </Head>
         <body>
           <Main />
