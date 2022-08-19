22e8ab3abe137a91f112b25cf2f8237bcc410d70
Tyler Menezes <tylermenezes@codeday.org>
2022-08-19 14:54:34 -0700

    Add GithubAuthors component

---
+++ src/Molecule/GithubAuthors/index.tsx
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from 'react';
+import { Image, Text, Box } from 'topo/Atom';
+import { apiFetch } from 'topo/utils';
+import { ChakraProps } from '@chakra-ui/react';
+
+const GITHUB_AUTHORS_QUERY = `
+query GithubAuthorsQuery($owner: String, $repository: String!, $path: String!, $branch: String) {
+  github {
+    contributors(owner: $owner, repository: $repository, path: $path, branch: $branch) {
+      account {
+        name
+        picture
+      }
+    }
+  }
+}`
+
+export interface GithubAuthorsProps extends ChakraProps {
+  owner?: string
+  repository: string
+  branch?: string
+  path: string
+}
+
+interface ContributorInfo {
+  account?: {
+    name: string
+    picture: string
+  }
+}
+
+export default function GithubAuthors({ owner, repository, branch, path,...props }: GithubAuthorsProps) {
+  const [authors, setAuthors] = useState<ContributorInfo[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      if (!repository || !path) return;
+      setLoading(true);
+      const resp = await apiFetch(GITHUB_AUTHORS_QUERY, { owner, repository, branch, path }, {});
+      setAuthors(resp?.github?.contributors || []);
+      setLoading(false);
+    })();
+  }, [owner, repository, branch, path]);
+
+  if (loading && repository && path) return <Box {...props}><Text d="inline">&nbsp;</Text></Box>
+  if (!repository || !path || (!authors || authors.length === 0)) return <></>;
+
+  return (
+    <Box {...props}>
+      <Text d="inline-block">Author{authors.length > 1 ? 's' : ''}:</Text>
+      {authors.filter((a) => a?.account?.name).slice(0,5).map((a) => (
+        <Box d="inline-block" pl={2}>
+          <Image d="inline" h="1em" rounded="full" alt="" mr={1} bgColor="white" src={a.account!.picture} />
+          {a.account!.name}
+        </Box>
+      ))}
+    </Box>
+  );
+}
